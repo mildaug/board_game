00931febedc6cea1fed7cf615283126fe77a1e27
Milda <auglyte.m@gmail.com>
2023-06-28 14:28:18 +0300

    basic board_ game model

--- board_game_hub/board_game/admin.py
+++ board_game_hub/board_game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Type)
+admin.site.register(models.Publisher)
+admin.site.register(models.Game)
--- board_game_hub/board_game/models.py
+++ board_game_hub/board_game/models.py
@@ -1,3 +1,81 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Type(models.Model):
+    name = models.CharField(_('name'), max_length=100, db_index=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('type')
+        verbose_name_plural = _('types')
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    def get_absolute_url(self):
+        return reverse('type_detail', kwargs={'pk': self.pk})
+    
+
+class Publisher(models.Model):
+    name = models.CharField(_('name'), max_length=100, db_index=True)
+    about_publisher = models.TextField(_('about_publisher'), max_length=1000)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('publisher')
+        verbose_name_plural = _('publishers')
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    def get_absolute_url(self):
+        return reverse('publisher_detail', kwargs={'pk': self.pk})
+
+
+class Game(models.Model):
+    user = models.ForeignKey(User, verbose_name=_('user'), on_delete=models.CASCADE, related_name='games')
+    title = models.CharField(_('title'), max_length=100, db_index=True)
+    publisher = models.ForeignKey(Publisher, verbose_name=_('publishers'), on_delete=models.CASCADE, related_name='games')
+    type = models.ManyToManyField(Type, verbose_name=_('type(s)'))
+    image = models.ImageField(_('image'), upload_to='board_game/game_images')
+    player_count = models.CharField(_('player_count'), max_length=50)
+    duration = models.CharField(_('duration'), max_length=50)
+    player_age = models.CharField(_('player_age'), max_length=50)
+
+    LANGUAGE_CHOICES = (
+        ('EN', _('EN')),
+        ('LT', _('LT')),
+        ('RU', _('RU')),
+        ('PL', _('PL')),
+        ('Other', _('Other')),
+    )
+
+    language = models.CharField(_('languages'), max_length=50, choices=LANGUAGE_CHOICES, default='LT', db_index=True)
+
+    DIFFICULTY_CHOICES = (
+        ('Easy', _('Easy')),
+        ('Medium', _('Medium')),
+        ('Hard', _('Hard')),
+        ('Extreme', _('Extreme')),
+        ('Nightmare', _('Nightmare')),
+    )
+
+    difficulty = models.CharField(_('difficulty'), max_length=50, choices=DIFFICULTY_CHOICES, default='Medium', db_index=True)
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = _('game')
+        verbose_name_plural = _('games')
+
+    def __str__(self):
+        return f'{self.title} {self.publisher} {self.type} {self.language} {self.difficulty}'
+    
+    def get_absolute_url(self):
+        return reverse('game_detail', kwargs={'pk': self.pk})
+    
