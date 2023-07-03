abc37a996d58866cef490b8ab0128f5d0407409e
Milda <auglyte.m@gmail.com>
2023-07-03 14:08:45 +0300

    minor changes

--- board_game_hub/user_profile/forms.py
+++ board_game_hub/user_profile/forms.py
@@ -11,7 +11,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email')
 
 
 class ProfileUpdateForm(forms.ModelForm):
