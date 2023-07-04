1ab2c205dea5a061cdd4d8f27e000724956220a4
Milda <auglyte.m@gmail.com>
2023-07-04 10:36:53 +0300

    changes in game borrow functionality

--- board_game_hub/board_game/admin.py
+++ board_game_hub/board_game/admin.py
@@ -7,13 +7,11 @@ class GameAdmin(admin.ModelAdmin):
 
 
 class GameBorrowRequestAdmin(admin.ModelAdmin):
-    list_display = ('game', 'borrower', 'owner', 'is_accepted')
+    list_display = ('game', 'borrower', 'owner')
 
 
 admin.site.register(models.Category)
 admin.site.register(models.Publisher)
 admin.site.register(models.Game, GameAdmin)
 admin.site.register(models.GameRating)
-admin.site.register(models.PartnerGameStore)
-admin.site.register(models.ParcelMachine)
 admin.site.register(models.GameBorrowRequest, GameBorrowRequestAdmin)
