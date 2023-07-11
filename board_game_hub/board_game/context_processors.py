2a509b3c8c319298d2cb9a6a84f9977eec079deb
Milda <auglyte.m@gmail.com>
2023-07-11 09:18:44 +0300

    notification circle bug repair

--- board_game_hub/board_game/context_processors.py
+++ board_game_hub/board_game/context_processors.py
@@ -2,7 +2,7 @@ from .models import GameBorrowRequest
 
 def new_requests_count(request):
     if request.user.is_authenticated:
-        new_requests_count = GameBorrowRequest.objects.filter(request_status='New').count()
+        new_requests_count = GameBorrowRequest.objects.filter(request_status='New', owner=request.user).count()
     else:
         new_requests_count = 0
 
